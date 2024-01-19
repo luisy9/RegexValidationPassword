b688ed11efdc1a0fb2747df8ad47e2b05f523f6b
Luis de Haro <luisdeharo4@gmail.com>
2024-01-19 12:06:46 +0100

    password

--- regex.js
+++ regex.js
@@ -1,11 +1,9 @@
 
 
-let regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*_\[\]:;<>,.?~\\-]).{8,}$/;
-
+let regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[_.&])[._&A-Za-z\d]{8,}$/;
 
 const PasswordValidation = (password) =>  console.log(regex.test(password));
 PasswordValidation('MiContraseña1234');
-
 
 //GMAIL 
 //^(?!^\.)[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-z0-9]+\.([a-z]{2})$
